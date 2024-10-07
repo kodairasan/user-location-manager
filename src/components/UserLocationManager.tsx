0744af209a31d355c7ec56766887f326b9fcce63
kodaira <kota020330@gmail.com>
2024-10-07 14:09:14 +0900

    選択肢の変更,gitignoreの追記

--- src/components/UserLocationManager.tsx
+++ src/components/UserLocationManager.tsx
@@ -14,7 +14,7 @@ type User = {
     details: string
 }
 
-const defaultLocations = ['407/408', '授業', 'リフレッシュルーム', '外出', '帰宅', 'その他']
+const defaultLocations = ['407/408/居室', '授業', 'リフレッシュルーム', '外出', '帰宅', 'その他']
 
 export default function UserLocationManager() {
     const [users, setUsers] = useState<User[]>([])
